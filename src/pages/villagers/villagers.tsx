8d10d399805153570473449a3ff2c5a9f08bde12
dev-hyeonmin <dev.hyeonmin@gmail.com>
2022-10-04 11:55:48 +0900

    Villagers :: css & paging

--- src/pages/villagers/villagers.tsx
+++ src/pages/villagers/villagers.tsx
@@ -25,32 +25,42 @@ export const Villagers = () => {
       });
   }, []);
 
+  const nextPage = () => {
+    setPage((currentPage) => currentPage + 1);
+  };
+
   return (
-    <div className="wrapper-villagers">
-      {villagers.slice(0, page * pageCount).map((villager, index) =>        
-        <Link key={villager.id + index} className="villager" to={`/villager/${villager.id}`} state={villager}>
-          <div className="vi-image" style={{backgroundImage: `url(${villager.image_url})`}}></div>
+    <>
+      <div className="wrapper-villagers">
+        {villagers.slice(0, page * pageCount).map((villager, index) =>        
+          <Link key={villager.id + index} className="villager" to={`/villager/${villager.id}`} state={villager}>
+            <div className="vi-image" style={{backgroundImage: `url(${villager.image_url})`}}></div>
 
-          <dl className="vi-info">
-            <dd>{villager.name}</dd>
-            <dd>
-              <img src={quote} />
-              {villager.quote}
-              <img src={quote} />
-            </dd>
-            <dd>
-                <span>#{getSpecies(villager.species)}</span>
-                <span>#{villager.gender}</span>
-                <span>#{getPersonality(villager.personality)}</span>
-            </dd>
-            <dd>🎉 {villager.birthday_month} / {villager.birthday_day}</dd>
-          </dl>
-        </Link>
-      )}
+            <dl className="vi-info">
+              <dd>{villager.name}</dd>
+              <dd>
+                <img src={quote} />
+                {villager.quote}
+                <img src={quote} />
+              </dd>
+              <dd>
+                  <span>#{getSpecies(villager.species)}</span>
+                  <span>#{getPersonality(villager.personality)}</span>
+                  <span>#{villager.gender}</span>
+              </dd>
+              <dd>🎉 {villager.birthday_month} / {villager.birthday_day}</dd>
+            </dl>
+          </Link>
+        )}        
 
-      {villagers.length === 0 && 
-          <div>Not Found.</div>
-      }
-    </div>
+        {villagers.length === 0 && 
+            <div>Not Found.</div>
+        }
+      </div>
+
+      <div className="box-button">
+        <button className="btn-page" onClick={nextPage}>more</button>
+      </div>
+    </>
   );
 }
